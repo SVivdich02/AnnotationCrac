6e36d189dfd930baf34b0a949c008717370e8077
SVivdich02 <vvv-sonya@mail.ru>
2022-05-15 19:12:51 +0300

    added methods of all superclasses

--- AnnotationProcessing/src/main/java/org/example/annotation/processor/BuilderProcessor.java
+++ AnnotationProcessing/src/main/java/org/example/annotation/processor/BuilderProcessor.java
@@ -28,48 +28,72 @@ public class BuilderProcessor extends AbstractProcessor {
             for (Element element : annotatedElements)
             {
                 LinkedHashMap<String, LinkedList<String>> methodsMap = new LinkedHashMap<>();
+                LinkedHashMap<String, String> mapMethodClass = new LinkedHashMap<>();
                 LinkedHashMap<String, String> returnTypeMap = new LinkedHashMap<>();
-
-                //String superClassName = element.getSimpleName().toString(); //-- это MyJButton
 
                 String superClassName = ((TypeElement)element).getSuperclass().toString();
                 String className = ((TypeElement) element).getQualifiedName().toString();
+
                 try {
-                    Class superClass = Class.forName(superClassName);
-                    for (Method method : superClass.getMethods())
-                    {
-                        int classModifiers = method.getModifiers();
-                        boolean isFinal = isModifierSet(classModifiers, Modifier.FINAL);
-                        boolean isStatic = isModifierSet(classModifiers, Modifier.STATIC);
-                        if (!isFinal & !isStatic) {
-                            LinkedList<String> listParamTypes = new LinkedList<>();
-                            for (Class cls : method.getParameterTypes())
-                            {
-                                listParamTypes.add(cls.getName());
-                            }
-                            methodsMap.put(method.getName(), listParamTypes);
-                            returnTypeMap.put(method.getName(), method.getReturnType().getName());
-                        }
-                    }
+                    Class startClass = Class.forName(superClassName);
+                    getMethodsMap(methodsMap, returnTypeMap, startClass, mapMethodClass);
 
                     try {
-                        generateCode(className, methodsMap, returnTypeMap, superClassName);
+                        generateCode(className, methodsMap, returnTypeMap, superClassName, mapMethodClass);
                     } catch (IOException e) {
                         e.printStackTrace();
                     }
+
                 } catch (ClassNotFoundException e) {
                     e.printStackTrace();
                 }
+
+                //String superClassName = element.getSimpleName().toString(); //-- это MyJButton
             }
         }
         return true;
+    }
+
+    public void getMethodsMap(LinkedHashMap<String, LinkedList<String>> methodsMap, LinkedHashMap<String, String> returnTypeMap, Class currentClass, LinkedHashMap<String, String> mapMethodClass) throws ClassNotFoundException {
+        while (true)
+        {
+            String currentClassName = currentClass.getName();
+            if (currentClassName.contains("Obj"))
+            {
+                break;
+            }
+            addMethodsOneClass(methodsMap, returnTypeMap, currentClass, mapMethodClass);
+
+            String superClassName = currentClass.getSuperclass().getName();
+            Class currentSuperClass = Class.forName(superClassName);
+            currentClass = currentSuperClass;
+        }
+    }
+
+    public void addMethodsOneClass(LinkedHashMap<String, LinkedList<String>> methodsMap, LinkedHashMap<String, String> returnTypeMap, Class currentClass, LinkedHashMap<String, String> mapMethodClass) {
+        for (Method method : currentClass.getMethods())
+        {
+            int classModifiers = method.getModifiers();
+            boolean isFinal = isModifierSet(classModifiers, Modifier.FINAL);
+            boolean isStatic = isModifierSet(classModifiers, Modifier.STATIC);
+            if (!isFinal & !isStatic) {
+                LinkedList<String> listParamTypes = new LinkedList<>();
+                for (Class cls : method.getParameterTypes())
+                {
+                    listParamTypes.add(cls.getName());
+                }
+                methodsMap.put(method.getName(), listParamTypes);
+                returnTypeMap.put(method.getName(), method.getReturnType().getName());
+                mapMethodClass.put(method.getName(), currentClass.getSimpleName());
+            }
+        }
     }
 
     public static boolean isModifierSet(int allModifiers, int specificModifier) {
         return (allModifiers & specificModifier) > 0;
     }
 
-    private void generateCode(String className, LinkedHashMap<String, LinkedList<String>> methodsMap, LinkedHashMap<String, String> returnTypeMap, String superClassName) throws IOException, ClassNotFoundException {
+    private void generateCode(String className, LinkedHashMap<String, LinkedList<String>> methodsMap, LinkedHashMap<String, String> returnTypeMap, String superClassName, LinkedHashMap<String, String> mapMethodClass) throws IOException, ClassNotFoundException {
         String packageName = null;
         int lastDot = className.lastIndexOf('.');
         if (lastDot > 0) {
@@ -98,10 +122,52 @@ public class BuilderProcessor extends AbstractProcessor {
 
             buildClassConstructor(out, generatedSimpleClassName, superClassName, generatedClassName);
 
+            // добавление метода getMap
+            out.println();
+            LinkedList<String> listMethodClass = new LinkedList<>();
+            listMethodClass.addAll(mapMethodClass.keySet());
+            Object[] valuesRes = mapMethodClass.values().toArray();
+
+            out.println();
+            out.println("""
+                        public LinkedHashMap<String, String> getMap() {
+                            LinkedHashMap<String, String> methodsMap = new LinkedHashMap<>();
+                    """);
+            for (int j = 0; j < mapMethodClass.size(); j++) {
+                out.println("String key" + j + " = \"" + listMethodClass.get(j) + "\";");
+                out.println("String value" + j + " = \"" + valuesRes[j] + "\";");
+                out.println("methodsMap.put(key" + j + ", value" + j + ");");
+            }
+            out.println("   return methodsMap;");
+            out.println("}");
+
+
+            // добавление метода add(Component) в MyPanelGenerated
+            if (generatedClassName.contains("Panel")) {
+                out.println();
+                out.println("""
+                            public java.awt.Component add(java.awt.Component comp) {
+                                StackTraceElement[] stackTraceElements = Thread.currentThread().getStackTrace();
+                                boolean fromMain = Arrays.stream(stackTraceElements).toList().get(2).getClassName().contains("Main");
+                                boolean fromSystem = Arrays.stream(stackTraceElements).toList().get(2).getClassName().contains("java.");
+                                boolean fromXSystem = Arrays.stream(stackTraceElements).toList().get(2).getClassName().contains("javax.");
+                                boolean fromSun = Arrays.stream(stackTraceElements).toList().get(2).getClassName().contains("sun");
+                            
+                            if (!fromSystem && !fromXSystem && !fromSun) {
+                                LinkedHashMap<Class, Object> paramValuesMap = new LinkedHashMap<>();
+                                paramValuesMap.put(java.awt.Component.class, comp);
+                        """);
+
+                out.print("GlobalList.list.add(new Operation(" + "\"" + generatedClassName + "\", \"" + "add" + "\", paramValuesMap, this.new" + superClassName.substring(13) + "));");
+                out.println("}");
+                out.println("   return super.add(comp);");
+                out.println("}");
+            }
+
             methodsMap.entrySet().forEach(method -> {
                 String methodName = method.getKey();
 
-                if (methodName != "createVolatileImage" && methodName != "createBufferStrategy") {
+                if (methodName != "createVolatileImage" && methodName != "createBufferStrategy" && methodName != "setFocusCycleRoot" && methodName != "getFocusCycleRootAncestor" &&methodName != "requestFocus") {
                     LinkedList<String> argumentType = method.getValue();
                     out.println("@Override");
                     out.println();
@@ -132,16 +198,19 @@ public class BuilderProcessor extends AbstractProcessor {
                             s = s.replace("$", ".");
                         }
                         out.print(s + " value" + i);
+
                         if (argumentType.size() != i) {
                             out.print(", ");
                         }
                     }
-
                     out.print(") {");
                     out.println();
 
-                    addOperationToList(out, argumentType, generatedClassName, methodName, superClassName);
+                    out.println("String fromClass = " + "\"" + mapMethodClass.get(methodName) + "\"" + ";");
 
+                    addOperationToList(out, argumentType, generatedClassName, methodName, superClassName, mapMethodClass);
+
+                    out.println();
                     if (newReturnTypeName != "void") {
                         out.print("   return super.");
                     } else {
@@ -152,6 +221,7 @@ public class BuilderProcessor extends AbstractProcessor {
                     int j = 0;
                     for (String s : argumentType) {
                         j++;
+
                         out.print("value" + j);
                         if (argumentType.size() != j) {
                             out.print(", ");
@@ -169,9 +239,7 @@ public class BuilderProcessor extends AbstractProcessor {
         }
     }
 
-
-    private void addOperationToList(PrintWriter out, LinkedList<String> argumentType, String generatedClassName, String methodName, String superClassName){
-
+    private void addOperationToList(PrintWriter out, LinkedList<String> argumentType, String generatedClassName, String methodName, String superClassName, LinkedHashMap<String, String> mapMethodsClass){
         out.println("""
                             StackTraceElement[] stackTraceElements = Thread.currentThread().getStackTrace();
                             boolean fromSystem = Arrays.stream(stackTraceElements).toList().get(2).getClassName().contains("java.");
@@ -180,7 +248,6 @@ public class BuilderProcessor extends AbstractProcessor {
                             
                             if (!fromSystem && !fromXSystem && !fromSun) {
                             """);
-
         int i = 0;
         out.println("   LinkedHashMap<Class, Object> paramValuesMap = new LinkedHashMap<>();");
         out.println();
